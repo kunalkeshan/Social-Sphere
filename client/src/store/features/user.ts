5cc02d4d79679dd527b5e8f02139b53cd43d2384
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-22 01:35:14 +0530

    chore: redux toolkit store setup

---
+++ client/src/store/features/user.ts
@@ -0,0 +1,11 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+	initialState: {},
+	name: 'user',
+	reducers: {},
+});
+
+// export const {} = userSlice.actions;
+
+export default userSlice.reducer;
