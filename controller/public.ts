f940696e89b68524cb7ee4f7f36de9e806b26e0d
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-21 18:20:00 +0530

    feat: fetch user profile endpoint added

---
+++ controller/public.ts
@@ -0,0 +1,29 @@
+/**
+ * Public Controller
+ */
+
+import Link from '../models/link';
+import User from '../models/user';
+import { UserProfileSchema } from '../schema/public';
+import { collections } from '../services/database';
+import { ApiError } from '../utils/apiError';
+
+// Dependencies
+
+export const userProfileController = async (data: UserProfileSchema) => {
+	const user = (await collections.users.findOne({
+		username: data.params.username,
+	})) as User;
+	if (!user)
+		throw new ApiError({
+			message: 'user/account-does-not-exists',
+			statusCode: 404,
+		});
+	const links = (await collections.links
+		.find({ userId: user.publicId })
+		.toArray()) as Link[];
+	delete user.password;
+	delete user.id;
+	delete user._id;
+	return { user, links };
+};
