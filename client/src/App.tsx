1a5bdc0a27d1b1af2aeb898afd116f3614afbf55
Kunal Keshan <kunalkeshan12@gmail.com>
2023-05-22 02:26:10 +0530

    Feat/Login page (#55)
    * chore: default code updated
    
    * chore: routes setup
    
    * chore: template login page complete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Landing from './pages/Landing';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Admin from './pages/admin/Admin';
+import PublicProfile from './pages/PublicProfile';
+import Main from './pages/admin/Main';
+import Settings from './pages/admin/Settings';
+import Appearance from './pages/admin/Appearance';
+import Analytics from './pages/admin/Analytics';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<Routes>
+			<Route path='/' element={<Landing />} />
+			<Route path='/login' element={<Login />} />
+			<Route path='/signup' element={<Signup />} />
+			<Route path='/admin' element={<Admin />}>
+				<Route index element={<Main />} />
+				<Route path='settings' element={<Settings />} />
+				<Route path='appearance' element={<Appearance />} />
+				<Route path='analytics' element={<Analytics />} />
+				<Route path='*' element={<Navigate to='/not-found' />} />
+			</Route>
+			<Route path='/:username' element={<PublicProfile />} />
+			<Route path='/not-found' element={<NotFound />} />
+		</Routes>
+	);
 }
 
-export default App
+export default App;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,109 @@
+import {
+	Grid,
+	CssBaseline,
+	Box,
+	Avatar,
+	Typography,
+	TextField,
+	Button,
+	Paper,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<Grid container component='main' sx={{ height: '100vh' }}>
+			<CssBaseline />
+			<Grid
+				item
+				xs={false}
+				sm={4}
+				md={7}
+				sx={{
+					backgroundImage: 'url(/login.svg)',
+					backgroundRepeat: 'no-repeat',
+					backgroundColor: (t) =>
+						t.palette.mode === 'light'
+							? t.palette.grey[50]
+							: t.palette.grey[900],
+					backgroundSize: 'contain',
+					backgroundPosition: 'center',
+				}}
+			/>
+			<Grid
+				item
+				xs={12}
+				sm={8}
+				md={5}
+				component={Paper}
+				elevation={6}
+				square
+			>
+				<Box
+					sx={{
+						my: 8,
+						mx: 4,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component='h1' variant='h5'>
+						Log in
+					</Typography>
+					<Box component='form' noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Email Address'
+							autoComplete='username'
+							type='text'
+							autoFocus
+						/>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Password'
+							type='password'
+							autoComplete='current-password'
+						/>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link
+									to='#'
+									className='underline underline-offset-2'
+								>
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link
+									to='#'
+									className='underline underline-offset-2'
+								>
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Login;
