08b3109b869c9c0309e6d9932a0da8f3b4129592
Kunal Keshan <kunalkeshan12@gmail.com>
2023-05-22 06:39:11 +0530

    Feat/Signup page (#56)
    * chore: signup page asset added
    
    * chore: login label updated
    
    * chore: signup page added

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,5 +1,119 @@
+import {
+	Grid,
+	CssBaseline,
+	Box,
+	Avatar,
+	Typography,
+	TextField,
+	Button,
+	Paper,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Link } from 'react-router-dom';
+
 const Signup = () => {
-	return <div>Signup</div>;
+	return (
+		<Grid container component='main' sx={{ height: '100vh' }}>
+			<CssBaseline />
+			<Grid
+				item
+				xs={false}
+				sm={4}
+				md={7}
+				sx={{
+					backgroundImage: 'url(/signup.svg)',
+					backgroundRepeat: 'no-repeat',
+					backgroundColor: (t) =>
+						t.palette.mode === 'light'
+							? t.palette.grey[50]
+							: t.palette.grey[900],
+					backgroundSize: '60% 60%',
+					backgroundPosition: 'center',
+				}}
+			/>
+			<Grid
+				item
+				xs={12}
+				sm={8}
+				md={5}
+				component={Paper}
+				elevation={6}
+				square
+			>
+				<Box
+					sx={{
+						my: 8,
+						mx: 4,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component='h1' variant='h5'>
+						Sign up
+					</Typography>
+					<Box component='form' noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Full Name'
+							autoComplete='full-name'
+							type='text'
+							autoFocus
+						/>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Email Address'
+							autoComplete='email'
+							type='email'
+							autoFocus
+						/>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Username'
+							autoComplete='username'
+							type='text'
+							autoFocus
+						/>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Password'
+							type='password'
+							autoComplete='current-password'
+						/>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item>
+								<Link
+									to='#'
+									className='underline underline-offset-2'
+								>
+									{'Already have an account? Log in'}
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Signup;
