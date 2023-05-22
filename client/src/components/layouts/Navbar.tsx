e9f741db11e257c8f8227873571878fae2aa1685
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-22 06:58:43 +0530

    chore: navbar and footer layout added

--- client/src/components/layouts/Navbar.tsx
+++ client/src/components/layouts/Navbar.tsx
@@ -1,5 +1,27 @@
+import { Box, AppBar, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<Box component={'nav'}>
+			<AppBar position='static' className='!bg-primary'>
+				<Toolbar
+					sx={{ maxWidth: 'xl' }}
+					className='w-full flex justify-between mx-auto'
+				>
+					<Typography
+						variant='h6'
+						component='a'
+						href='/'
+						sx={{ flexGrow: 1, fontFamily: 'Syne' }}
+					>
+						Social Sphere
+					</Typography>
+					<Link to={'/login'}>Login / Signup</Link>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 };
 
 export default Navbar;
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,5 +1,13 @@
+import Footer from '../components/layouts/Footer';
+import Navbar from '../components/layouts/Navbar';
+
 const Landing = () => {
-	return <div>Landing</div>;
+	return (
+		<>
+			<Navbar />
+			<Footer />
+		</>
+	);
 };
 
 export default Landing;
