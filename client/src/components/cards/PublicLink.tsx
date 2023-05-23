de5929b23317303af85d40273da2108864c9fdec
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-23 18:31:28 +0530

    chore: public page links section added

--- client/src/components/cards/PublicLink.tsx
+++ client/src/components/cards/PublicLink.tsx
@@ -1,5 +1,25 @@
-const PublicLink = () => {
-	return <div>PublicLink</div>;
+import React from 'react';
+import { Link as ILink } from '../../../../@types';
+import IosShareIcon from '@mui/icons-material/IosShare';
+
+type PublicLinkProps = ILink;
+
+const PublicLink: React.FC<PublicLinkProps> = ({ title, description, url }) => {
+	return (
+		<a
+			href={url}
+			target='_blank'
+			className='bg-white text-black max-w-3xl w-full mx-auto rounded px-8 py-4 group transistion-all hover:scale-105 duration-300'
+		>
+			<div className='w-full flex items-center justify-between'>
+				<div className='flex flex-col'>
+					<span className='font-heading font-bold'>{title}</span>
+					<span className='text-sm'>{description}</span>
+				</div>
+				<IosShareIcon className='!invisible group-hover:!visible transition-all duration-300' />
+			</div>
+		</a>
+	);
 };
 
 export default PublicLink;
--- client/src/pages/PublicProfile.tsx
+++ client/src/pages/PublicProfile.tsx
@@ -2,6 +2,7 @@ import ShareIcon from '@mui/icons-material/Share';
 import { Link, User } from '../../../@types/';
 import { IconButton } from '@mui/material';
 import PublicSocials from '../components/cards/PublicSocials';
+import PublicLink from '../components/cards/PublicLink';
 
 const PublicProfile = () => {
 	const PROFILE: User = {
@@ -20,7 +21,28 @@ const PublicProfile = () => {
 		},
 	};
 
-	const LINKS: Link[] = [];
+	const LINKS: Link[] = [
+		{
+			title: 'Blog',
+			description: 'Visit to learn tech from a different perspective.',
+			url: 'https://blog.kunalkeshan.dev/',
+			userId: '93e06117-b471-474d-99b3-bebc231ba481',
+		},
+		{
+			title: 'ECE Notes Initative',
+			description:
+				'Collections of notes for ECE dept. from 2020-24 - SRMIST.',
+			url: 'https://notes.kunalkeshan.dev/',
+			userId: '93e06117-b471-474d-99b3-bebc231ba481',
+		},
+		{
+			title: 'Shiryoku (Resources)',
+			description:
+				'Open Source Collection of Resources to upskill in any field.',
+			url: 'https://resources.kunalkeshan.dev/',
+			userId: '93e06117-b471-474d-99b3-bebc231ba481',
+		},
+	];
 
 	return (
 		<main className='max-w-5xl mx-auto w-full px-4 py-8 grid grid-cols-1 gap-8'>
@@ -32,21 +54,29 @@ const PublicProfile = () => {
 					<ShareIcon className='text-white' />
 				</IconButton>
 			</section>
-			<section className=''>
-				<header className='flex items-center justify-center flex-col gap-2'>
-					<div className='max-w-[8rem] rounded-full overflow-hidden'>
-						<img
-							src={PROFILE.avatar}
-							alt={PROFILE.fullName}
-							className='w-full h-auto object-contain'
-						/>
-					</div>
-					<h1 className='font-heading text-3xl'>{PROFILE.title}</h1>
-					<h2 className=''>{PROFILE.bio}</h2>
-				</header>
+			<section className='flex items-center justify-center flex-col gap-2'>
+				<div className='max-w-[8rem] rounded-full overflow-hidden'>
+					<img
+						src={PROFILE.avatar}
+						alt={PROFILE.fullName}
+						className='w-full h-auto object-contain'
+					/>
+				</div>
+				<h1 className='font-heading text-3xl'>{PROFILE.title}</h1>
+				<h2 className=''>{PROFILE.bio}</h2>
 			</section>
-			<section className='flex items-center justify-center w-full'>
+			<section className='my-8'>
+				<div className='grid grid-cols-1 gap-6'>
+					{LINKS.map((link, index) => (
+						<PublicLink key={index} {...link} />
+					))}
+				</div>
+			</section>
+			<section className='flex items-center justify-center w-full flex-col gap-2'>
 				<PublicSocials socials={PROFILE.socials} />
+				<p className='font-heading text-sm font-semibold'>
+					&copy; Social Sphere - {new Date().getFullYear()}
+				</p>
 			</section>
 		</main>
 	);
