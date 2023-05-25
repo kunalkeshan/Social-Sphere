44efd4457bf15a655fc1bfcf276823891a001ca3
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-25 12:48:21 +0530

    chore: analytics page added - under contruction

--- client/src/pages/admin/Analytics.tsx
+++ client/src/pages/admin/Analytics.tsx
@@ -1,5 +1,15 @@
+import UnderConstruction from '../../components/reusable/UnderConstruction';
+
 const Analytics = () => {
-	return <div>Analytics</div>;
+	return (
+		<section className='col-span-3 md:col-span-2 md:border-r border-gray-500 px-2 md:px-8'>
+			<h1 className='font-heading text-3xl'>Analytics</h1>
+			<hr className='w-full mt-2 border-b border-b-gray-500' />
+			<div>
+				<UnderConstruction />
+			</div>
+		</section>
+	);
 };
 
 export default Analytics;
