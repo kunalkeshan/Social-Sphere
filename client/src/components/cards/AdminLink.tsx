dc5fee89f9091f7cb102af53590f3daf7cbd6a4e
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-28 12:04:10 +0530

    chore: delete button added to admin link card

--- client/src/components/cards/AdminLink.tsx
+++ client/src/components/cards/AdminLink.tsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Link as ILink } from '../../../../@types';
 import EditIcon from '@mui/icons-material/Edit';
 import DoneIcon from '@mui/icons-material/Done';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface PublicLinkProps extends ILink {
 	index: number;
@@ -53,6 +54,12 @@ const AdminLink: React.FC<PublicLinkProps> = ({
 		<div className='flex flex-col px-8 py-4 border-2 border-white gap-2 w-full relative'>
 			<span className='absolute bg-white font-heading font-bold text-lg -top-2 -left-2 text-primary rounded-full p-2 w-6 h-6 flex items-center justify-center -rotate-12'>
 				{index + 1}
+			</span>
+			<span
+				title='Delete Link'
+				className='absolute bg-white font-heading font-bold text-lg -bottom-2 -right-2 text-primary rounded-full p-2 w-6 h-6 flex items-center justify-center rotate-12 curosr-pointer hover:scale-110 transition-all cursor-pointer hover:text-red-500'
+			>
+				<DeleteIcon />
 			</span>
 			<div className='w-full flex items-center justify-between gap-2'>
 				<input
