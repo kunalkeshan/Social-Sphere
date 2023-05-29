560066d3471e97e314a9c739fdba9d741a808741
kunalkeshan <kunalkeshan12@gmail.com>
2023-05-29 12:02:35 +0530

    chore: middleware documentation added

--- middleware/morgan.ts
+++ middleware/morgan.ts
@@ -1,10 +1,17 @@
+/**
+ * Custom HTTP Logger Middleware
+ */
+
+// Dependencies
 import morgan, { StreamOptions } from 'morgan';
 import Logger from '../utils/logger';
 
+// Use Winston http log method for requests
 const stream: StreamOptions = {
 	write: (message) => Logger.http(message),
 };
 
+// Skip if in production
 const skip = () => {
 	const env = process.env.NODE_ENV || 'development';
 	return env !== 'development';
