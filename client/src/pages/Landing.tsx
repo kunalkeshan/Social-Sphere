f4b978c20943c07ad581294927edc83b0a3f215f
Ahad1713 <khanazan761@gmail.com>
2023-10-24 00:21:09 +0530

    reform code

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -7,8 +7,6 @@ import Features from '../components/landing/Features';
 import Intro from '../components/landing/Intro';
 import Footer from '../components/layouts/Footer';
 import Navbar from '../components/layouts/Navbar';
-import ForgotPasswordPage from './ForgotPassword';
-import Login from './Login';
 
 const Landing = () => {
 	return (
