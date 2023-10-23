973e37f64c0f788d0f2ac0f553d4a9f603126d1f
Ahad1713 <khanazan761@gmail.com>
2023-10-23 12:54:51 +0530

    reform code

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -7,16 +7,12 @@ import Features from '../components/landing/Features';
 import Intro from '../components/landing/Intro';
 import Footer from '../components/layouts/Footer';
 import Navbar from '../components/layouts/Navbar';
+import ForgotPasswordPage from './ForgotPassword';
 
 const Landing = () => {
 	return (
 		<>
-			<Navbar />
-			<main className='w-full px-5'>
-				<Intro />
-				<Features />
-			</main>
-			<Footer />
+			<ForgotPasswordPage/>
 		</>
 	);
 };
