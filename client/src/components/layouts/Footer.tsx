11bef310fbf6591e090726f2cd648ed9f3839856
Disha <dishay166@gmail.com>
2023-10-24 00:16:16 +0530

    chore: add link to repo

--- client/src/components/layouts/Footer.tsx
+++ client/src/components/layouts/Footer.tsx
@@ -3,26 +3,41 @@
  */
 
 // Dependencies
-import { Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-	return (
-		<Box className='w-full my-6' component={'footer'}>
-			<Box
-				component={'p'}
-				sx={{ maxWidth: 'xl' }}
-				className='mx-auto text-center'
-			>
-				{'Copyright © '}
-				<Link to='/' className='font-heading'>
-					Social Sphere
-				</Link>{' '}
-				{new Date().getFullYear()}
-				{'.'}
-			</Box>
-		</Box>
-	);
+  return (
+    <Box className="w-full my-6" component={"footer"}>
+      <Box
+        component={"p"}
+        sx={{ maxWidth: "xl" }}
+        className="mx-auto text-center"
+      >
+        {"Copyright © "}
+        <Link to="/" className="font-heading">
+          Social Sphere
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+        <a
+          href="https://kunalkeshan.dev/"
+          className="font-heading"
+          target="_blank"
+        >
+          Made with ❤️ by <u>Kunal Keshan</u>
+        </a>
+        <br />
+        <a
+          href="https://github.com/kunalkeshan/Shiryoku"
+          className="font-heading"
+          target="_blank"
+        >
+          <u>Repo</u>
+        </a>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
